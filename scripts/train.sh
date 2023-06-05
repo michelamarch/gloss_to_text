a23e7bd68b4d42012e333efbf8b3adf140a79839
Ubuntu <ubuntu@ip-172-31-8-18.us-west-1.compute.internal>
2023-06-05 00:11:46 +0000

    moved files around

--- scripts/train.sh
+++ scripts/train.sh
@@ -1,14 +1,14 @@
 python -m torch.distributed.launch --nproc_per_node=4 --master_port=12233 --use_env run_train.py \
---diff_steps 2000 \
+--diff_steps 20 \
 --lr 0.0001 \
---learning_steps 50000 \
---save_interval 10000 \
+--learning_steps 500 \
+--save_interval 100 \
 --seed 102 \
 --noise_schedule sqrt \
 --hidden_dim 128 \
 --bsz 2048 \
---dataset qqp \
---data_dir {path-to-datasets} \
+--dataset gloss \
+--data_dir ../datasets/gloss_data \
 --vocab bert \
 --seq_len 128 \
 --schedule_sampler lossaware \
